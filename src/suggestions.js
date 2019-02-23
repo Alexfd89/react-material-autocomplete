c15c431ec415c6f9f21bdabf4942b915fe4fbac0
unknown <Alexfd89>
2019-02-23 17:47:58 +0200

    init data

--- src/suggestions.js
+++ src/suggestions.js
@@ -1,5 +1,5 @@
 export default [
-    { name: 'Alex Labs', address: 'alex.f@testerlabs.com', isValid: true },
+    { name: 'Alex Labs', address: 'alex@example.com', isValid: true },
     { name: 'Mike Arts', address: 'mike@gmail.com', isValid: true },
     { name: 'Abra Kadabra', address: 'abrakadabra@ggg', isValid: false },
   ];
